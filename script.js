c0ec341beea47f6dd2e779dded808f1a84826607
Sarthak Goyal <82680055+shadow-14@users.noreply.github.com>
2025-06-13 15:12:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function makeid(l) {
-  // write your code here
+ let result = '';
+    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    
+    // Loop to generate characters for the specified length
+    for (let i = 0; i < length; i++) {
+        const randomInd = Math.floor(Math.random() * characters.length);
+        result += characters.charAt(randomInd);
+    }
+    return result;
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
+ const l = prompt("Enter a number.");
 alert(makeid(l));
